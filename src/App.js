42d6f7fc1c4c58b93573a6d56224ae372ba4d516
Portobello <lamija.selmanovic@gmail.com>
2018-09-03 19:55:32 +0200

    Remove unnecessary and make first component

--- src/App.js
+++ src/App.js
@@ -2,17 +2,46 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const menuItems = {
+  hr: [
+    "Ljepota i Vi",
+    "Kuhanje na zdrav način",
+    "Kampiranje na otvorenom",
+    "Aktivirajte se",
+    "Meditacija",
+    "2 + 2 = 3",
+    "Za filmofile i knjigoljupce",
+    "Jučer danas sutra"
+  ],
+  en: [
+    "Beauty and You",
+    "Healthy Cooking",
+    "Outdoor Camping",
+    "Activate",
+    "Meditation",
+    "2 + 2 = 3",
+    "Cinephiles' and Booklovers' Corner",
+    "Yesterday Today Tomorrow"
+  ]
+};
+
+class NavBar extends Component {
+  render() {
+    return (
+      <header className="App-header">
+        <ul>
+          <li>Test</li>
+        </ul>
+      </header>
+    )
+  }
+}
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <NavBar />
       </div>
     );
   }
