3f9fac87eb7c5d81ace82a35973d965217c243a8
Portobello <lamija.selmanovic@gmail.com>
2018-09-03 21:36:09 +0200

    select language and style menu item titles

--- src/App.js
+++ src/App.js
@@ -3,22 +3,58 @@ import logo from './logo.svg';
 import './App.css';
 import {menuItems} from './config.js';
 
-class MenuItems extends Component {
+class SelectLanguage extends Component {
+  render() {
+    return (
+      <select defaultValue="hr">
+        <option key="hr" value="hr">HR</option>
+        <option key="en" value="en">EN</option>
+      </select>
+    )
+  }
+}
+
+function WordBreaker({title, breakAt = 3}) {
+  return title && title.split(' ').map(word => word.length <= breakAt ? `${word} ` : <React.Fragment key={word}>{word}<br /></React.Fragment>)
+}
+
+class MenuItems extends React.Component {
+  // constructor(props) {
+  //   super(props);
+  // }
   render() {
     return (
       <ul>
-        <li>{this.props.items[1]}</li>
-        <li>{this.props.items[2]}</li>
+        {this.props.items.map(item => <li ref={item.ref} key={item.text}><WordBreaker title={item.text} /></li>)}
       </ul>
     )
   }
 }
 
+function prepareItems (items) {
+  return items.map(item => ({
+    text: item
+  }))
+}
+
 class NavBar extends Component {
-  render() {
+  constructor (props) {
+    super(props)
+
+    const defaultLanguage = 'hr'
+    const items = props.items[defaultLanguage]
+
+    this.state = {
+      language: defaultLanguage,
+      items: prepareItems(items)
+    }
+  }
+
+  render () {
     return (
       <header className="App-header">
-        <MenuItems items={menuItems.hr}/>
+        <MenuItems items={this.state.items} />
+        <SelectLanguage />
       </header>
     )
   }
@@ -28,7 +64,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <NavBar />
+        <NavBar items={menuItems} />
       </div>
     );
   }
