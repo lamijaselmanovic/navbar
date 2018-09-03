8d4e45509a95851cf5da5c7319063a8bf5b7d5e8
Portobello <lamija.selmanovic@gmail.com>
2018-09-03 20:07:18 +0200

    show test menu items

--- src/App.js
+++ src/App.js
@@ -1,37 +1,24 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {menuItems} from './config.js';
 
-const menuItems = {
-  hr: [
-    "Ljepota i Vi",
-    "Kuhanje na zdrav način",
-    "Kampiranje na otvorenom",
-    "Aktivirajte se",
-    "Meditacija",
-    "2 + 2 = 3",
-    "Za filmofile i knjigoljupce",
-    "Jučer danas sutra"
-  ],
-  en: [
-    "Beauty and You",
-    "Healthy Cooking",
-    "Outdoor Camping",
-    "Activate",
-    "Meditation",
-    "2 + 2 = 3",
-    "Cinephiles' and Booklovers' Corner",
-    "Yesterday Today Tomorrow"
-  ]
-};
+class MenuItems extends Component {
+  render() {
+    return (
+      <ul>
+        <li>{this.props.items[1]}</li>
+        <li>{this.props.items[2]}</li>
+      </ul>
+    )
+  }
+}
 
 class NavBar extends Component {
   render() {
     return (
       <header className="App-header">
-        <ul>
-          <li>Test</li>
-        </ul>
+        <MenuItems items={menuItems.hr}/>
       </header>
     )
   }
---
+++ src/config.js
@@ -0,0 +1,22 @@
+export const menuItems = {
+  hr: [
+    "Ljepota i Vi",
+    "Kuhanje na zdrav način",
+    "Kampiranje na otvorenom",
+    "Aktivirajte se",
+    "Meditacija",
+    "2 + 2 = 3",
+    "Za filmofile i knjigoljupce",
+    "Jučer danas sutra"
+  ],
+  en: [
+    "Beauty and You",
+    "Healthy Cooking",
+    "Outdoor Camping",
+    "Activate",
+    "Meditation",
+    "2 + 2 = 3",
+    "Cinephiles' and Booklovers' Corner",
+    "Yesterday Today Tomorrow"
+  ]
+};
