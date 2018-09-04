d56008a3787e59956dcffc16028284c49ce47539
Portobello <lamija.selmanovic@gmail.com>
2018-09-04 20:09:28 +0200

    Remove li items by priority and show them in more menu

--- src/App.js
+++ src/App.js
@@ -19,27 +19,28 @@ function WordBreaker({title, breakAt = 3}) {
   return title && title.split(' ').map(word => word.length <= breakAt ? `${word} ` : <React.Fragment key={word}>{word}<br /></React.Fragment>)
 }
 
-class ShowMore extends Component {
-  render() {
-    return (
-      <ul id="more">
-        <li>{menuItems.hr[1]}</li>
-        <li>{menuItems.hr[2]}</li>
-      </ul>
-    )
-  }
-}
+// class ShowMore extends Component {
+//   render() {
+//     return (
+//       <ul id="more">
+//         <li>{menuItems.hr[1]}</li>
+//         <li>{menuItems.hr[2]}</li>
+//       </ul>
+//     )
+//   }
+// }
+
+// {this.props.hiddenArray.map(item => <li key={item.text}><WordBreaker title={item.text} />Lamija</li>)}
 
 class MenuItems extends Component {
-  // constructor(props) {
-  //   super(props);
-  // }
   render() {
     return (
       <ul id="menu-items">
         {this.props.items.map(item => <li ref={item.ref} key={item.text}><WordBreaker title={item.text} /></li>)}
         <li>...
-          <ShowMore />
+          <ul id="more">
+            {this.props.hiddenArray.map(item => <li key={item.text}><WordBreaker title={item.text} /></li>)}
+          </ul>
         </li>
       </ul>
     )
@@ -61,10 +62,14 @@ class NavBar extends Component {
 
     const defaultLanguage = 'hr'
     const items = props.items[defaultLanguage]
+    const hiddenArray = []
+    const shownArray = []
 
     this.state = {
       language: defaultLanguage,
-      items: prepareItems(items)
+      items: prepareItems(items),
+      hiddenArray: hiddenArray,
+      shownArray: shownArray
     }
 
     this.onWindowResize = this.onWindowResize.bind(this)
@@ -87,31 +92,55 @@ class NavBar extends Component {
   }
 
   onWindowResize (event) {
-    console.warn('window resized', event.target.outerWidth)
+    // console.warn('window resized', event.target.outerWidth)
 
     const sumOfAllWidths = this.state.items.reduce((sum, item) => sum + item.width, 0)
 
+    // console.warn('sum of all', sumOfAllWidths)
+    // console.warn('razlika', event.target.outerWidth - sumOfAllWidths)
 
-    const difference = event.target.outerWidth - sumOfAllWidths
-
+    // 100 od selecta
+    const difference = event.target.outerWidth - sumOfAllWidths + 100
     
     const items = this.state.items.map(function(item) {
       return {
         ...item,
         width: item.ref.current.offsetWidth
       }
-    }).filter(function(item) {
-      if (item.width >= difference) return false
-      return true
     })
 
-    this.setState({items: items})
+    const hiddenArray = this.state.hiddenArray
+    const shownArray = items
+    
+    // const screenWidth = 400;
+    // 130 of select language - fix this..
+    const screenWidth = event.target.outerWidth - 130;
+
+    const removeExcessItems = (items, cutOff) => {
+      const cutOffItems = items.slice(0, cutOff)
+      const sumOfWidths = cutOffItems.reduce((widthSum, {width}) => widthSum + width, 0)
+      if (sumOfWidths < screenWidth) {
+        return cutOffItems
+      }
+      else {
+        hiddenArray.push(items[items.length-1])
+        shownArray.slice(items[items.length-1])
+        // console.log(hiddenArray);
+        // console.log(shownArray);
+        // console.log(items.length)
+        return removeExcessItems(items, cutOff ? cutOff - 1 : -1)
+      }
+    }
+
+    this.setState({items: removeExcessItems(items)})
+    this.setState({hiddenArray: hiddenArray})
+    this.setState({shownArray: shownArray})
   }
 
   render () {
     return (
       <header className="App-header">
-        <MenuItems items={this.state.items} />
+        <MenuItems items={this.state.items} hiddenArray={this.state.hiddenArray}/>
         <SelectLangage onChange={language => this.setState({language})} />
       </header>
     )
